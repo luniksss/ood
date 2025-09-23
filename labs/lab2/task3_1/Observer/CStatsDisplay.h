b16fd5fe8fd2000248654077c4d052f133ed959e
luniksss <nikolaeval316@gmail.com>
2025-09-23 20:10:59 +0300

    добавила изменения в 3 задание 2 лабы

--- labs/lab2/task3_1/Observer/CStatsDisplay.h
+++ labs/lab2/task3_1/Observer/CStatsDisplay.h
@@ -9,12 +9,20 @@
 class CStatsDisplay : public IObserver<SWeatherInfo>
 {
 private:
-    void calculateData(SWeatherInfo const& data)
+    void CalculateData(SWeatherInfo const& data)
     {
         m_temperature.ProcessData(data.temperature);
         m_humidity.ProcessData(data.humidity);
         m_pressure.ProcessData(data.pressure);
     }
+
+    static void DisplayData(const CCounter &data, const std::string& name)
+    {
+        std::cout << "Max " << name << " " << data.GetMaxData() << std::endl;
+        std::cout << "Min " << name << " " << data.GetMinData() << std::endl;
+        std::cout << "Average " << name << " " << data.GetAverage() << std::endl;
+    }
+
 
     /* Метод Update сделан приватным, чтобы ограничить возможность его вызова напрямую
     Классу CObservable он будет доступен все равно, т.к. в интерфейсе IObserver он
@@ -22,11 +30,11 @@ private:
     */
     void Update(SWeatherInfo const& data) override
     {
-        calculateData(data);
+        CalculateData(data);
 
-        m_temperature.DisplayData("Temperature");
-        m_humidity.DisplayData("Humidity");
-        m_pressure.DisplayData("Pressure");
+        DisplayData(m_temperature, "Temperature");
+        DisplayData(m_humidity, "Humidity");
+        DisplayData(m_pressure, "Pressure");
         std::cout << "----------------" << std::endl;
     }
 
